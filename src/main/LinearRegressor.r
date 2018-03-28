a42e29c4df6d3b9f74330a489d7efb998411d6bf
Adail Carvalho <jcarvalho@idealit.com.br>
2018-03-28 14:45:15 -0300

    Adds method to calculate RMSE and simple linear model coefficients.

--- src/main/LinearRegressor.r
+++ src/main/LinearRegressor.r
@@ -29,7 +29,7 @@ notSupportedException <- simpleError("Operation with the passed args are not sup
                $Coefficients : list containing the values founded for the linear model coeficients 
                $Predictions  : matrix that shows the predicted values, the real values for Y and the E residuals
                $RMSErrorRate : rating used to know how good our model is. Value is always between -1< R <=1.
-
+               $PossibleOutliersNumLines: show lines containing both min anx max residuals values.
  # usage
 
  beearModel <- linearModelMain("C:/opt/beer_delivery.csv","\t", "Tempo", "NumCaixas", "Distancia")
@@ -49,7 +49,7 @@ linearModelMain <- function (filePath, delimiter, dependentVar, ...) {
   
   matrixDataSet <<- getFileToMatrix(filePath, delimiter)
   if (length(independentVarList) == 1) {
-    resultModel <<- simpleLinearModel(matrixDataSet, dependentVar)
+    resultModel <<- simpleLinearModel(matrixDataSet, dependentVar, independentVarList)
   } else {
     resultModel <<- multiLinearModel(matrixDataSet, dependentVar, independentVarList)    
   }
@@ -61,24 +61,64 @@ getFileToMatrix <- function(filePath, delimiter) {
   
 }
 
-simpleLinearModel <- function (matrixDataSet, dependentVar) {
+# Handle general operations to get the results
+# @returns results A list containing the coefficient values, RMSE error and the made predictions.
+simpleLinearModel <- function (matrixDataSet, dependentVar, independentVarList) {
   if (!is.matrix(matrixDataSet)) {
     stop(illegalArgsException)
   }
- 
-  dependentIndex <- grep(dependentVar, colnames(matrixDataSet))
-  dependentMatrix <<- matrixDataSet[,dependentIndex]
-  independentMatrix <<- matrixDataSet[,-dependentIndex]
   
-  # Generates a matrix that holds the mean of each X variable in the dataset.
+  independentVar <- independentVarList[[1]]
+  dependentIndex <- grep(dependentVar, colnames(matrixDataSet))
+  independentIndex <- grep(independentVar, colnames(matrixDataSet))
+  
+  dependentVector <<- matrixDataSet[,dependentIndex]
+  independentVector <<- matrixDataSet[,independentIndex]
+  
+  # Calculates the numerator of the Simple Linear Regression equation to find B1 coeficient value.
+  levelsVectorX <- independentVector - mean(independentVector)
+  levelsVectorY <- dependentVector - mean(dependentVector)
+
+  
+  # Returns a vetor that holds the mean of each X or Y variable in the dataset.
   # x - ~x ~> ~x = avg(x)
-  levelsMatrizX <- getRegressionLevelsMatrix(independentMatrix)
   # y - ~y ~> ~y = avg(y)
-  levelsMatrixY <- getRegressionLevelsMatrix(dependentMatrix)
-  print(levelsMatrizX)
-  print(levelsMatrixY)
+  mult <- levelsVectorX * levelsVectorY
+  numeratorB1 <- sum(mult)
+  
+  # Calculates the denominator of the Simple Linear Regression equation to find B1 coeficient value.
+  denominatorB1 <- sum(levelsVectorX ** 2)
+  
+  # Finds the B1 final value
+  coefficientB1 <- numeratorB1 / denominatorB1
+  
+  # With B1 value calculated, we can now find the value of B0.
+  coefficientB0 <- mean(dependentVector) - (coefficientB1 * mean(independentVector))
+  
+  coefficientsSLM <- matrix(nrow = 1, ncol = 2, dimnames = list(c(1),c("Coefficient_B0", "Coefficient_B1")))
+  coefficientsSLM[,1] <- coefficientB0
+  coefficientsSLM[,2] <- coefficientB1
+  
+  resultModel <- createSimpleRegressionModel(coefficientB0, coefficientB1, independentVector, dependentVector)
+    
+  # Calculates the Error rate using Root Main Square Error
+  modelErrorRate <- estimateError(resultModel[,2])
+  
+  linearModel <- cbind(dependentVector,resultModel)
+  colnames(linearModel) <- c("Real_Y","Predict_Y", "Resid_E")
+  
+  possibleOutLiers <- getPossibleOutliersLines(resultModel[,2])
+  outLiersMatrix <- matrix(possibleOutLiers, nrow = 2, ncol = 2, dimnames = list(c("Min_Val_Line_Num","Max_Val_Line_Num"), c("OutLiersLines", "Value")))
+  
+  # Gather generetade values
+  results <- list(coefficientsSLM, linearModel, modelErrorRate, outLiersMatrix)
+  names(results) <-c("Coefficients", "Predictions", "RMSErrorRate","PossibleOutliersNumLines")
+  
+  returnValue(results)
 }
 
+# Handle general operations to get the results
+# @returns results A list containing the coefficient values, RMSE error and the made predictions.  
 multiLinearModel <- function (matrixDataSet, dependentVar, independentVarList) {
   if (!is.matrix(matrixDataSet)) {
     stop(illegalArgsException)
@@ -118,14 +158,17 @@ multiLinearModel <- function (matrixDataSet, dependentVar, independentVarList) {
   linearModel <- cbind(dependentMatrix,resultModel)
   colnames(linearModel) <- c("Real_Y","Predict_Y", "Resid_E")
   
+  possibleOutLiers <- getPossibleOutliersLines(resultModel[,2])
+  outLiersMatrix <- matrix(possibleOutLiers, nrow = 2, ncol = 2, dimnames = list(c("Min_Val_Line_Num","Max_Val_Line_Num"), c("OutLiersLines", "Value")))
+  
   # Gather generetade values
-  results <- list(coeficientsMatrix, linearModel, modelErrorRate)
-  names(results) <-c("Coefficients", "Predictions", "RMSErrorRate")
+  results <- list(coeficientsMatrix, linearModel, modelErrorRate, outLiersMatrix)
+  names(results) <-c("Coefficients", "Predictions", "RMSErrorRate", "PossibleOutliersNumLines")
   
   returnValue(results)
 }
 
-# Returns the mean of independent var.
+# Returns the mean of independent variables in multivariant linear model
 getRegressionLevelsMatrix <- function(matriz) {
   avgIndependentsVarMatrix <<- t(as.matrix(colMeans(matriz)))
   levelsMatrix <- cbind(getX0Dimension(matriz), matriz)
@@ -176,12 +219,29 @@ matrixMultiply <- function(matrixA, matrixB) {
 }
 
 # Estimates the values for Y, using the calculated coefficients.
+# Apllies the following rule: ~Yi : B0 + (B1 * Xi)
+# Where ~Yi = estimated value for dependent value Y, B0 = Slope coefficient, B1 = Intercept coefficient, Xi = observed value for X, 
+# and i, the iterator value over the X independent vector
+# returns a Matrix with the estimated values for Y and the residuals E, where E = real value of Y - estimated value for Y
+createSimpleRegressionModel <- function(coefficientB0, coefficientB1, independentVector, dependentVector) {
+  predictedY <- vector("numeric", length(dependentVector))
+  residuals <- vector("numeric", length(independentVector))
+  
+  for (i in 1:length(independentVector)) {
+    predictedY[i] <- coefficientB0 + (coefficientB1 * independentVector[i])
+  }
+  
+  residualVector <- dependentVector - predictedY
+  returnValue(cbind(predictedY, residualVector))
+}
+
+# Estimates the values for Y, using the calculated coefficients.
 # Apllies the following rule: ~Y : B0 + B1(X1 - ~X1) + B2(X2 - ~X2) + ... + Bn(Xn - ~Xn)
 # Where ~Y = estimated value for dependent value Y, Bi = Coeficient values, Xi = observed value for X, ~Xi = mean of X variable.
 # returns a Matrix with the estimated values for Y and the residuals E, where E = real value of Y - estimated value for Y
 createRegressionModel <- function(coefficientsMatrix, independentMatrix, dependentMatrix, levelsMatrix) {
   predictedY <- matrix(0, nrow = nrow(independentMatrix), ncol = 1)
-  residuals <- matrix(0 , nrow = nrow(independentMatrix), ncol = 1)
+  residualMatrix <- matrix(0 , nrow = nrow(independentMatrix), ncol = 1)
   
   
   for (r in 1:nrow(independentMatrix)) {
@@ -194,9 +254,9 @@ createRegressionModel <- function(coefficientsMatrix, independentMatrix, depende
     }
   }
   
-  residuals <- dependentMatrix - predictedY;
+  residualMatrix <- dependentMatrix - predictedY;
 
-  returnValue(cbind(predictedY, residuals))
+  returnValue(cbind(predictedY, residualMatrix))
 }
 
 # Returns the (matriz)-¹ of a given matrix.
@@ -220,5 +280,16 @@ treatInfValuesAsZeros <- function(matriz) {
 # Estimate erros using RootMeanSquareError(RMSE).
 # @residualVector Vector that holds the E residuals of our model.
 estimateError <- function(residualVector) {
-  returnValue(sqrt(sum(resid ** 2) / length(residualVector)))
+  rmse <- sqrt(sum(residualVector ** 2) / length(residualVector))
+  
+  returnValue(rmse)
+}
+
+# Helps identifiend the lines where the residuals got more outfitted of our model 
+getPossibleOutliersLines <- function(residualVector) {
+  maxResidual <- max(residualVector)
+  minResidual <- min(residualVector)
+  maxResidualLineIndex <- match(maxResidual, residualVector)
+  minResidualLineIndex <- match(minResidual, residualVector)
+  returnValue(c(minResidualLineIndex, maxResidualLineIndex, maxResidual, minResidual))
 }
