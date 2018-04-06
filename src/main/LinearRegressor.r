37fbfdcff91f5d892e045b7f2e0eb5b2eab43604
Adail Carvalho <jcarvalho@idealit.com.br>
2018-04-06 16:44:49 -0300

    Fixes matrix inversion and slope calculation

--- src/main/LinearRegressor.r
+++ src/main/LinearRegressor.r
@@ -4,10 +4,6 @@
   # __________________________________
   
   #Requires ====
-  
-  #install.packages("matlib")
-  library(matlib)
-  
   # Matrices / DFrames headers ====
   resultsHeader <- c("Coefficients", "Predictions", "RMSErrorRate", "Dataset")
   lmHeader <- c("Real_Y","Predict_Y", "Resid_E")
@@ -76,7 +72,7 @@
     slope <- getSLRSlope(independentVector, dependentVector)
     intercept <- getSLRIntercept(independentVector, dependentVector, slope)
     
-    resultModel <- createSimpleRegressionModel(intercept, slope, independentVector, dependentVector)
+    resultModel <- doSimplePredictions(intercept, slope, independentVector, dependentVector)
     
     coefficientsMatrix <- matrix(nrow = 1, ncol = 2, dimnames = list(c(1),c("Intercept", independentVar)))
     coefficientsMatrix[,1] <- intercept
@@ -107,55 +103,40 @@
       stop(illegalArgsException)
     }
     
-    #Identifies which column represents the dependent variable. Separates the X variables and Y
-    #And store in different matrices.
     dependentColIndex <- grep(dependentVar, colnames(matrixDataSet))
     dependentMatrix <- matrixDataSet[,dependentColIndex]
     
-    independentMatrix <- matrix(0, nrow = nrow(matrixDataSet), ncol = length(independentVarList))
-    colnames(independentMatrix) <- independentVarList
-    for (i in independentVarList) {
-      name <- grep(i, colnames(matrixDataSet))
-      independentMatrix[,i] <- matrixDataSet[,name]  
-    }
+    independentMatrix <- getIndependentVarsAsMatrix(matrixDataSet, independentVarList)
     
-    # Generates a matrix that holds the mean of each X variable in the dataset.
-    # x - ~x ~> ~x = avg(x)
     levelsMatrix <- getRegressionLevelsMatrix(as.matrix(independentMatrix))
     
     xProductMatrix <- matrixTranposeAndMultiply(levelsMatrix, NULL)
-    xProductMatrix <- treatInfValuesAsZeros(xProductMatrix)
     xProductMatrix <- getInverseMatrix(xProductMatrix)
     yProductMatrix <- matrixTranposeAndMultiply(levelsMatrix, as.matrix(dependentMatrix))
-   
-    # Calculates the coefficients
-    coeficientsMatrix <- matrixMultiply(xProductMatrix,yProductMatrix)
-    coeficientsMatrix <- as.matrix(coeficientsMatrix[,1])
     
-    # Estimates Y values
-    resultModel <- createRegressionModel(coeficientsMatrix, independentMatrix, dependentMatrix, levelsMatrix)
+    coefficientsMatrix <- getMLRCoefficients(xProductMatrix, yProductMatrix, independentMatrix, independentVarList)
     
-    # Calculates the Error rate using Root Main Square Error
+    resultModel <- doMultivariantPredictions(coefficientsMatrix, independentMatrix, levelsMatrix, dependentMatrix)
+    
     modelErrorRate <- estimateModelError(resultModel[,2])
     
     linearModel <- cbind(dependentMatrix,resultModel)
     colnames(linearModel) <- lmHeader
-    
-    # Gather generetade values
-    results <- list(coeficientsMatrix, linearModel, modelErrorRate, matrixDataSet)
+  
+    results <- list(coefficientsMatrix, linearModel, modelErrorRate, matrixDataSet)
     names(results) <- resultsHeader
     
     returnValue(results)
   }
   
-  # Linear Model Creation ====
+  # Linear Model ====
   
   # Estimates the values for Y, using the calculated coefficients.
   # Apllies the following rule: ~Yi : B0 + (B1 * Xi)
   # Where ~Yi = estimated value for dependent value Y, B0 = Slope coefficient, B1 = Intercept coefficient, Xi = observed value for X, 
   # and i, the iterator value over the X independent vector
   # returns a Matrix with the estimated values for Y and the residuals E, where E = real value of Y - estimated value for Y
-  createSimpleRegressionModel <- function(coefficientB0, coefficientB1, independentVector, dependentVector) {
+  doSimplePredictions <- function(coefficientB0, coefficientB1, independentVector, dependentVector) {
     predictedY <- vector("numeric", length(dependentVector))
     residuals <- vector("numeric", length(independentVector))
     
@@ -171,31 +152,52 @@
   # Apllies the following rule: ~Y : B0 + B1(X1 - ~X1) + B2(X2 - ~X2) + ... + Bn(Xn - ~Xn)
   # Where ~Y = estimated value for dependent value Y, Bi = Coeficient values, Xi = observed value for X, ~Xi = mean of X variable.
   # returns a Matrix with the estimated values for Y and the residuals E, where E = real value of Y - estimated value for Y
-  createRegressionModel <- function(coefficientsMatrix, independentMatrix, dependentMatrix, levelsMatrix) {
+  doMultivariantPredictions <- function(coefficientsMatrix, independentMatrix, levelsMatrix, dependentMatrix = NULL) {
     predictedY <- matrix(0, nrow = nrow(independentMatrix), ncol = 1)
     residualMatrix <- matrix(0 , nrow = nrow(independentMatrix), ncol = 1)
     
-    
     for (r in 1:nrow(independentMatrix)) {
-      for (coe in 1:nrow(coefficientsMatrix)) {
+      for (coe in 1:ncol(coefficientsMatrix)) {
         if (coe == 1) {
-          predictedY[r,1] <- predictedY[r,1] + coefficientsMatrix[coe,]
+          predictedY[r,1] <- predictedY[r,1] + coefficientsMatrix[,coe]
         } else {
-          predictedY[r,1] <- predictedY[r,1] + (coefficientsMatrix[coe,] * levelsMatrix[r,coe])
+          predictedY[r,1] <- predictedY[r,1] + (coefficientsMatrix[,coe] * independentMatrix[r,coe - 1])
         }
       }
     }
     
-    residualMatrix <- dependentMatrix - predictedY;
+    predictions <-NULL
+    if (missing(dependentMatrix)) {
+      predictions <- predictedY 
+    } else {
+      residualMatrix <- dependentMatrix - predictedY
+      predictions <- cbind(predictedY, residualMatrix)
+    }
     
-    returnValue(cbind(predictedY, residualMatrix))
+    returnValue(predictions)
   }
   
   # Matrices general operations ====
+  getMLRCoefficients <- function(xProductMatrix, yProductMatrix, independentMatrix, xLabels) {
+    avgIndMat <- colMeans(independentMatrix)
+    
+    coefficientsMatrix <- matrixMultiply(xProductMatrix, yProductMatrix)
+    coefficientsMatrix <- t(coefficientsMatrix)
+    slope <- coefficientsMatrix[,1]
+    
+    for(c in 2:ncol(coefficientsMatrix)) {
+      slope <- slope + (coefficientsMatrix[,c] * (-avgIndMat[c - 1]))
+    }
+    coefficientsMatrix[,1] <- slope
+    colnames(coefficientsMatrix) <-c("[Intercept]", xLabels)
+    returnValue(coefficientsMatrix)
+    
+  }
   # Returns the (matriz)-¹ of a given matrix.
   # inv(x) is a funtion provided by the package matlib*
   getInverseMatrix <- function(matriz) {
-    returnValue(inv(matriz))
+    invertedMatrix <- solve(matriz)
+    returnValue(invertedMatrix)
   }
   
   # Returns the mean of independent variables in multivariant linear model
@@ -249,18 +251,6 @@
     returnValue(matrixMultiply(transpMatrix, matrixY))
   }
   
-  # Substitutes Inf values for zeros
-  treatInfValuesAsZeros <- function(matriz) {
-    for (c in 1:ncol(matriz)) {
-      for (r in 1:nrow(matriz)) {
-        if (is.infinite(matriz[c,r])) {
-          matriz[c,r] <- 0
-        } 
-      }
-    }
-    returnValue(matriz) 
-  }
-  
   # Linear Regression Rules Application ====
   # Estimate erros using RootMeanSquareError(RMSE).
   # @residualVector Vector that holds the E residuals of our model.
@@ -288,6 +278,17 @@
   }
   
   # Utils ====
+  getIndependentVarsAsMatrix <- function(matrixData, independentVarList) {
+    independentMatrix <- matrix(0, nrow = nrow(matrixData), ncol = length(independentVarList))
+    colnames(independentMatrix) <- independentVarList
+    
+    for (i in independentVarList) {
+      name <- grep(i, colnames(matrixData))
+      independentMatrix[,i] <- matrixData[,name]  
+    }
+    returnValue(independentMatrix)
+  }
+  
   getFileToMatrix <- function(filePath, delimiter) {
     rawDataSet <-  read.csv(filePath, sep = delimiter, header = TRUE)
     returnValue(data.matrix(rawDataSet))
